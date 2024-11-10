8987d65d58aa6ecfe57fb9bf9f9ab30b412c86b6
Chad <2770198+chadian@users.noreply.github.com>
2024-11-10 10:26:04 -0500

    Clean up and reorganize

--- src/board.cr
+++ src/board.cr
@@ -1,5 +1,6 @@
 require "colorize"
 require "./piece"
+require "./spatial"
 
 # TODO
 # - [ ] Out of board checks (can't run off the bounds of the board)
@@ -14,137 +15,6 @@ PieceCharacterSymbol = {
   King:   "♚",
 }
 
-PieceCharacterLetter = {
-  Pawn: "P",
-  Rook: "R",
-  # Fix this, Knight and King can't both be 'K'
-  Knight: "K",
-  Bishop: "B",
-  Queen:  "Q",
-  King:   "K",
-}
-
-alias BoardCoordinate = {Char, Int32}
-alias ArrayMatrixCoordinate = {Int32, Int32}
-
-FileNumber = {
-  a: 1,
-  b: 2,
-  c: 3,
-  d: 4,
-  e: 5,
-  f: 6,
-  g: 7,
-  h: 8,
-}
-
-def convert_board_coordinate_to_array_matrix_coordinate(coordinate : BoardCoordinate) : ArrayMatrixCoordinate
-  file = coordinate[0]
-  # Ranks are 1-indexed and start from the bottom left where the ArrayMatrix structure
-  # starts at the top left. So the 8th rank is is actually the 0th index and the 1st rank
-  # is the the 0th
-  rank = 8 - (coordinate[1])
-  file_number = FileNumber[file.downcase.to_s] - 1
-
-  array_matrix_range = 0..7
-  if (!(array_matrix_range.includes?(file_number)) || !(array_matrix_range.includes?(rank)))
-    raise "BoardCoordinate #{coordinate} is out of range of the ArrayMatrixCoordinate limits #{array_matrix_range}"
-  end
-
-  {rank, file_number}
-end
-
-def create_movement(from : BoardCoordinate, to : BoardCoordinate) : PieceMovement
-  from_rank_number = from[1]
-  from_file_number = FileNumber[from[0].to_s]
-  to_rank_number = to[1]
-  to_file_number = FileNumber[to[0].to_s]
-
-  horizontal_direction : Direction::E | Direction::W | Nil = nil
-  vertical_direction : Direction::N | Direction::S | Nil = nil
-  count : Int32 | Jump
-
-  horizontal_diff = (from_file_number - to_file_number).abs
-  vertical_diff = (from_rank_number - to_rank_number).abs
-
-  if horizontal_diff != 0
-    horizontal_direction = from_file_number < to_file_number ? Direction::E : Direction::W
-  end
-
-  if vertical_diff != 0
-    vertical_direction = from_rank_number < to_rank_number ? Direction::N : Direction::S
-  end
-
-  is_diagonal_move = !horizontal_direction.nil? && !vertical_direction.nil?
-  is_knight_jump = (horizontal_diff == 2 && vertical_diff == 1) || (horizontal_diff == 1 && vertical_diff == 2)
-
-  if is_knight_jump
-    knight_moves = 4
-    knight_movement = case {vertical_direction, vertical_diff, horizontal_direction, horizontal_diff}
-                      when {Direction::N, 2, Direction::E, 1}
-                        {Direction::NE, Jump::LONG}.as PieceMovement
-                      when {Direction::N, 1, Direction::E, 2}
-                        {Direction::NE, Jump::SHORT}.as PieceMovement
-                      when {Direction::N, 2, Direction::W, 1}
-                        {Direction::NW, Jump::LONG}.as PieceMovement
-                      when {Direction::N, 1, Direction::W, 2}
-                        {Direction::NW, Jump::SHORT}.as PieceMovement
-                      when {Direction::S, 2, Direction::E, 1}
-                        {Direction::SE, Jump::LONG}.as PieceMovement
-                      when {Direction::S, 1, Direction::E, 2}
-                        {Direction::SE, Jump::SHORT}.as PieceMovement
-                      when {Direction::S, 2, Direction::W, 1}
-                        {Direction::SW, Jump::LONG}.as PieceMovement
-                      when {Direction::S, 1, Direction::W, 2}
-                        {Direction::SW, Jump::SHORT}.as PieceMovement
-                      end
-
-    if knight_movement.nil?
-      raise "Could not determine Knight's movement"
-    end
-
-    return knight_movement
-  end
-
-  if is_diagonal_move
-    if horizontal_diff == vertical_diff
-      # for non-knights, a valid diagonal in chess is 1:1 for horizontal to
-      #  vertical squares a diagonal move counts squares on the diagonal,
-      # instead of combining its separate horizontal and vertical counts,
-      # and because it's 1:1 horizontal to vertical the diagonal movement is same for either
-      count = horizontal_diff
-    else
-      raise "Illegal diagonal move"
-    end
-  else
-    # either the horizontal_diff is 0 or the vertical_diff is 0
-    # since the direction if not diagonal is one along one axis,
-    # and therefore the total moves can be added
-    count = horizontal_diff + vertical_diff
-  end
-
-  case {vertical_direction, horizontal_direction}
-  when {Direction::N, nil}
-    {Direction::N, count}
-  when {Direction::N, Direction::E}
-    {Direction::NE, count}
-  when {nil, Direction::E}
-    {Direction::E, count}
-  when {Direction::S, Direction::E}
-    {Direction::SE, count}
-  when {Direction::S, nil}
-    {Direction::S, count}
-  when {Direction::S, Direction::W}
-    {Direction::SW, count}
-  when {nil, Direction::W}
-    {Direction::W, count}
-  when {Direction::N, Direction::W}
-    {Direction::NW, count}
-  else
-    raise "No direction found"
-  end
-end
-
 class Board
   getter structure
 
@@ -153,6 +23,7 @@ class Board
     @structure = Array(Array(Piece | Nil)).new(8) { Array(Piece | Nil).new(8, nil) }
   end
 
+  # TODO: Convert this to a `BoardCoordinate` input
   def add_piece(x : Number, y : Number, piece : Piece)
     @structure[x][y] = piece
   end
--- src/piece.cr
+++ src/piece.cr
@@ -1,3 +1,5 @@
+require "./spatial"
+
 enum PieceColor
   White
   Black
@@ -6,8 +8,6 @@ enum PieceColor
     self == PieceColor::White ? PieceColor::Black : PieceColor::White
   end
 end
-
-alias PieceCoordinate = {Int32, Int32}
 
 enum Direction
   N
@@ -36,21 +36,6 @@ abstract class Piece
   getter color : PieceColor
 
   def initialize(@color, @board : Board)
-  end
-
-  def coordinates : PieceCoordinate | Nil
-    @board.structure.each_index do |rank_index|
-      rank = @board.structure[rank_index]
-
-      rank.each_index do |file_index|
-        piece = rank[file_index]
-        if (self == piece)
-          return {rank_index, file_index}
-        end
-      end
-    end
-
-    return nil
   end
 
   abstract def moves : Array(PieceMovement)
