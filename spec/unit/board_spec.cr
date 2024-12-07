ff1c3f181db5056ef739442b41d112d0e778b6f8
Chad <2770198+chadian@users.noreply.github.com>
2024-12-07 09:50:40 -0500

    Move #add_piece tests into a describe block

--- spec/unit/board_spec.cr
+++ spec/unit/board_spec.cr
@@ -51,40 +51,42 @@ describe "Board" do
     board.structure.should eq empty_board
   end
 
-  it "#add_piece can add a piece at specific coordinates" do
-    board = Board.new
+  describe "#add_piece" do
+    it "can add a piece at specific coordinates" do
+      board = Board.new
 
-    top_left_piece = Pawn.new(PieceColor::White)
-    bottom_left_piece = Pawn.new(PieceColor::White)
-    top_right_piece = Pawn.new(PieceColor::White)
-    bottom_right_piece = Pawn.new(PieceColor::White)
+      top_left_piece = Pawn.new(PieceColor::White)
+      bottom_left_piece = Pawn.new(PieceColor::White)
+      top_right_piece = Pawn.new(PieceColor::White)
+      bottom_right_piece = Pawn.new(PieceColor::White)
 
-    board.add_piece({'a', 1}, bottom_left_piece)
-    board.add_piece({'a', 8}, top_left_piece)
-    board.add_piece({'h', 8}, top_right_piece)
-    board.add_piece({'h', 1}, bottom_right_piece)
+      board.add_piece({'a', 1}, bottom_left_piece)
+      board.add_piece({'a', 8}, top_left_piece)
+      board.add_piece({'h', 8}, top_right_piece)
+      board.add_piece({'h', 1}, bottom_right_piece)
 
-    board.structure.should eq [
-      [top_left_piece, nil, nil, nil, nil, nil, nil, top_right_piece],
-      [nil, nil, nil, nil, nil, nil, nil, nil],
-      [nil, nil, nil, nil, nil, nil, nil, nil],
-      [nil, nil, nil, nil, nil, nil, nil, nil],
-      [nil, nil, nil, nil, nil, nil, nil, nil],
-      [nil, nil, nil, nil, nil, nil, nil, nil],
-      [nil, nil, nil, nil, nil, nil, nil, nil],
-      [bottom_left_piece, nil, nil, nil, nil, nil, nil, bottom_right_piece],
-    ]
-  end
+      board.structure.should eq [
+        [top_left_piece, nil, nil, nil, nil, nil, nil, top_right_piece],
+        [nil, nil, nil, nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil, nil, nil, nil],
+        [bottom_left_piece, nil, nil, nil, nil, nil, nil, bottom_right_piece],
+      ]
+    end
 
-  it "#add_piece raises when a piece already exists" do
-    board = Board.new
-    piece = Pawn.new(PieceColor::White)
-    board.add_piece({'a', 1}, piece)
+    it "raises when a piece already exists" do
+      board = Board.new
+      piece = Pawn.new(PieceColor::White)
+      board.add_piece({'a', 1}, piece)
 
-    another_piece = Pawn.new(PieceColor::White)
+      another_piece = Pawn.new(PieceColor::White)
 
-    expect_raises(Exception) do
-      board.add_piece({'a', 1}, another_piece)
+      expect_raises(Exception) do
+        board.add_piece({'a', 1}, another_piece)
+      end
     end
   end
 
