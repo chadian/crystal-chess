c1de2302fc433186b50b9b0352dd9428a4423991
Chad <2770198+chadian@users.noreply.github.com>
2024-12-09 12:42:11 -0500

    Add #setup_board with tests

--- spec/unit/game_spec.cr
+++ spec/unit/game_spec.cr
@@ -11,4 +11,88 @@ describe "Game" do
     game = Game.new
     (game.board.is_a? Board).should be_true
   end
+
+  describe "#setup_board" do
+    describe "it setups the board correctly" do
+      it "has all the white pieces in the correct location" do
+        game = Game.new
+        game.setup_board
+
+        (game.board.piece_at_coordinate({'a', 1}).is_a? Rook).should be_true
+        (game.board.piece_at_coordinate({'b', 1}).is_a? Knight).should be_true
+        (game.board.piece_at_coordinate({'c', 1}).is_a? Bishop).should be_true
+        (game.board.piece_at_coordinate({'d', 1}).is_a? Queen).should be_true
+        (game.board.piece_at_coordinate({'e', 1}).is_a? King).should be_true
+        (game.board.piece_at_coordinate({'f', 1}).is_a? Bishop).should be_true
+        (game.board.piece_at_coordinate({'g', 1}).is_a? Knight).should be_true
+        (game.board.piece_at_coordinate({'h', 1}).is_a? Rook).should be_true
+
+        (game.board.piece_at_coordinate({'a', 2}).is_a? Pawn).should be_true
+        (game.board.piece_at_coordinate({'b', 2}).is_a? Pawn).should be_true
+        (game.board.piece_at_coordinate({'c', 2}).is_a? Pawn).should be_true
+        (game.board.piece_at_coordinate({'d', 2}).is_a? Pawn).should be_true
+        (game.board.piece_at_coordinate({'e', 2}).is_a? Pawn).should be_true
+        (game.board.piece_at_coordinate({'f', 2}).is_a? Pawn).should be_true
+        (game.board.piece_at_coordinate({'g', 2}).is_a? Pawn).should be_true
+        (game.board.piece_at_coordinate({'h', 2}).is_a? Pawn).should be_true
+
+        ((game.board.piece_at_coordinate({'a', 1}).as Piece).color).should eq PieceColor::White
+        ((game.board.piece_at_coordinate({'b', 1}).as Piece).color).should eq PieceColor::White
+        ((game.board.piece_at_coordinate({'c', 1}).as Piece).color).should eq PieceColor::White
+        ((game.board.piece_at_coordinate({'d', 1}).as Piece).color).should eq PieceColor::White
+        ((game.board.piece_at_coordinate({'e', 1}).as Piece).color).should eq PieceColor::White
+        ((game.board.piece_at_coordinate({'f', 1}).as Piece).color).should eq PieceColor::White
+        ((game.board.piece_at_coordinate({'g', 1}).as Piece).color).should eq PieceColor::White
+        ((game.board.piece_at_coordinate({'h', 1}).as Piece).color).should eq PieceColor::White
+        ((game.board.piece_at_coordinate({'a', 2}).as Piece).color).should eq PieceColor::White
+        ((game.board.piece_at_coordinate({'b', 2}).as Piece).color).should eq PieceColor::White
+        ((game.board.piece_at_coordinate({'c', 2}).as Piece).color).should eq PieceColor::White
+        ((game.board.piece_at_coordinate({'d', 2}).as Piece).color).should eq PieceColor::White
+        ((game.board.piece_at_coordinate({'e', 2}).as Piece).color).should eq PieceColor::White
+        ((game.board.piece_at_coordinate({'f', 2}).as Piece).color).should eq PieceColor::White
+        ((game.board.piece_at_coordinate({'g', 2}).as Piece).color).should eq PieceColor::White
+        ((game.board.piece_at_coordinate({'h', 2}).as Piece).color).should eq PieceColor::White
+      end
+
+      it "has all the black pieces in the correct location" do
+        game = Game.new
+        game.setup_board
+
+        (game.board.piece_at_coordinate({'a', 8}).is_a? Rook).should be_true
+        (game.board.piece_at_coordinate({'b', 8}).is_a? Knight).should be_true
+        (game.board.piece_at_coordinate({'c', 8}).is_a? Bishop).should be_true
+        (game.board.piece_at_coordinate({'d', 8}).is_a? Queen).should be_true
+        (game.board.piece_at_coordinate({'e', 8}).is_a? King).should be_true
+        (game.board.piece_at_coordinate({'f', 8}).is_a? Bishop).should be_true
+        (game.board.piece_at_coordinate({'g', 8}).is_a? Knight).should be_true
+        (game.board.piece_at_coordinate({'h', 8}).is_a? Rook).should be_true
+
+        (game.board.piece_at_coordinate({'a', 7}).is_a? Pawn).should be_true
+        (game.board.piece_at_coordinate({'b', 7}).is_a? Pawn).should be_true
+        (game.board.piece_at_coordinate({'c', 7}).is_a? Pawn).should be_true
+        (game.board.piece_at_coordinate({'d', 7}).is_a? Pawn).should be_true
+        (game.board.piece_at_coordinate({'e', 7}).is_a? Pawn).should be_true
+        (game.board.piece_at_coordinate({'f', 7}).is_a? Pawn).should be_true
+        (game.board.piece_at_coordinate({'g', 7}).is_a? Pawn).should be_true
+        (game.board.piece_at_coordinate({'h', 7}).is_a? Pawn).should be_true
+
+        ((game.board.piece_at_coordinate({'a', 7}).as Piece).color).should eq PieceColor::Black
+        ((game.board.piece_at_coordinate({'b', 7}).as Piece).color).should eq PieceColor::Black
+        ((game.board.piece_at_coordinate({'c', 7}).as Piece).color).should eq PieceColor::Black
+        ((game.board.piece_at_coordinate({'d', 7}).as Piece).color).should eq PieceColor::Black
+        ((game.board.piece_at_coordinate({'e', 7}).as Piece).color).should eq PieceColor::Black
+        ((game.board.piece_at_coordinate({'f', 7}).as Piece).color).should eq PieceColor::Black
+        ((game.board.piece_at_coordinate({'g', 7}).as Piece).color).should eq PieceColor::Black
+        ((game.board.piece_at_coordinate({'h', 7}).as Piece).color).should eq PieceColor::Black
+        ((game.board.piece_at_coordinate({'a', 8}).as Piece).color).should eq PieceColor::Black
+        ((game.board.piece_at_coordinate({'b', 8}).as Piece).color).should eq PieceColor::Black
+        ((game.board.piece_at_coordinate({'c', 8}).as Piece).color).should eq PieceColor::Black
+        ((game.board.piece_at_coordinate({'d', 8}).as Piece).color).should eq PieceColor::Black
+        ((game.board.piece_at_coordinate({'e', 8}).as Piece).color).should eq PieceColor::Black
+        ((game.board.piece_at_coordinate({'f', 8}).as Piece).color).should eq PieceColor::Black
+        ((game.board.piece_at_coordinate({'g', 8}).as Piece).color).should eq PieceColor::Black
+        ((game.board.piece_at_coordinate({'h', 8}).as Piece).color).should eq PieceColor::Black
+      end
+    end
+  end
 end
--- src/game.cr
+++ src/game.cr
@@ -1,7 +1,54 @@
 require "./board"
 
 class Game
+  getter board
+
   def initialize
     @board = Board.new
+  end
+
+  def setup_board
+    add_starting_white_pieces
+    add_starting_black_pieces
+  end
+
+  private def add_starting_white_pieces
+    board.add_piece({'a', 1}, Rook.new(PieceColor::White))
+    board.add_piece({'b', 1}, Knight.new(PieceColor::White))
+    board.add_piece({'c', 1}, Bishop.new(PieceColor::White))
+    board.add_piece({'d', 1}, Queen.new(PieceColor::White))
+    board.add_piece({'e', 1}, King.new(PieceColor::White))
+    board.add_piece({'f', 1}, Bishop.new(PieceColor::White))
+    board.add_piece({'g', 1}, Knight.new(PieceColor::White))
+    board.add_piece({'h', 1}, Rook.new(PieceColor::White))
+
+    board.add_piece({'a', 2}, Pawn.new(PieceColor::White))
+    board.add_piece({'b', 2}, Pawn.new(PieceColor::White))
+    board.add_piece({'c', 2}, Pawn.new(PieceColor::White))
+    board.add_piece({'d', 2}, Pawn.new(PieceColor::White))
+    board.add_piece({'e', 2}, Pawn.new(PieceColor::White))
+    board.add_piece({'f', 2}, Pawn.new(PieceColor::White))
+    board.add_piece({'g', 2}, Pawn.new(PieceColor::White))
+    board.add_piece({'h', 2}, Pawn.new(PieceColor::White))
+  end
+
+  private def add_starting_black_pieces
+    board.add_piece({'a', 7}, Pawn.new(PieceColor::Black))
+    board.add_piece({'b', 7}, Pawn.new(PieceColor::Black))
+    board.add_piece({'c', 7}, Pawn.new(PieceColor::Black))
+    board.add_piece({'d', 7}, Pawn.new(PieceColor::Black))
+    board.add_piece({'e', 7}, Pawn.new(PieceColor::Black))
+    board.add_piece({'f', 7}, Pawn.new(PieceColor::Black))
+    board.add_piece({'g', 7}, Pawn.new(PieceColor::Black))
+    board.add_piece({'h', 7}, Pawn.new(PieceColor::Black))
+
+    board.add_piece({'a', 8}, Rook.new(PieceColor::Black))
+    board.add_piece({'b', 8}, Knight.new(PieceColor::Black))
+    board.add_piece({'c', 8}, Bishop.new(PieceColor::Black))
+    board.add_piece({'d', 8}, Queen.new(PieceColor::Black))
+    board.add_piece({'e', 8}, King.new(PieceColor::Black))
+    board.add_piece({'f', 8}, Bishop.new(PieceColor::Black))
+    board.add_piece({'g', 8}, Knight.new(PieceColor::Black))
+    board.add_piece({'h', 8}, Rook.new(PieceColor::Black))
   end
 end
